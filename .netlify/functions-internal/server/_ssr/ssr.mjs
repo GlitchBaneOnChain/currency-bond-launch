c1d93acb9e4972eb1ccb1f58d223d2265f98dd95
MYSN <MYSN@Mac.localdomain>
2026-09-08 02:54:30 +0100

    Hide Netlify branding badge injected on the site

--- .netlify/functions-internal/server/_ssr/ssr.mjs
+++ .netlify/functions-internal/server/_ssr/ssr.mjs
@@ -93,7 +93,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-CcpCvj_x.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-ChXXyaT2.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
